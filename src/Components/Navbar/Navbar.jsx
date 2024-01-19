56b53d96fda2df6a9c45223f745c0823d24ff4ef
sandy <2007206it@cit.edu.in>
2024-01-19 19:59:21 +0530

    Initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <div className="navContainer">
+            <span className="logo">E-booking</span>
+            <div className="navItems">
+                <button className="navButton">Register</button>
+                <button className="navButton">Login</button>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
